98eb9e0ffb77f455e6946e513dffb6f345d63261
godchurch <37278203+godchurch@users.noreply.github.com>
2019-09-05 13:16:55 -1000

    mount points for chroot

---
+++ mount.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e -x
+
+if test -z "$1"; then
+  printf "Usage: %s [chroot directory]" "$0"
+  exit 1
+fi
+CHROOT="$(realpath "$1")"
+
+if test -d "$CHROOT"; then
+  mount proc   "$CHROOT/proc"    -t proc     -o nosuid,nodev,noexec
+  mount sys    "$CHROOT/sys"     -t sysfs    -o nosuid,nodev,noexec,ro
+  mount udev   "$CHROOT/dev"     -t devtmpfs -o mode=0755,nosuid
+  mount devpts "$CHROOT/dev/pts" -t devpts   -o mode=0620,gid=5,nosuid,noexec
+  mount shm    "$CHROOT/dev/shm" -t tmpfs    -o mode=1777,nosuid,nodev
+  mount run    "$CHROOT/run"     -t tmpfs    -o mode=0755,nosuid,nodev
+  mount tmp    "$CHROOT/tmp"     -t tmpfs    -o mode=1777,strictatime,nodev,nosuid
+fi
