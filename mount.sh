742a736df65f08a4f6904113e538c3918d515c0d
godchurch <37278203+godchurch@users.noreply.github.com>
2020-06-04 00:28:27 -1000

    fix chroot variable

--- mount.sh
+++ mount.sh
@@ -3,10 +3,10 @@
 set -e
 test -z "$1" && { printf "Usage: %s [chroot directory]" "$0"; exit 1; }
 set -x
-mountpoint "$TARGET"
-test -d "$TARGET"
-mount -t proc proc "$TARGET/proc"
-mount -t sysfs sysfs "$TARGET/sys"
-mount -t tmpfs run "$TARGET/run"
-mkdir -p "$TARGET/run/systemd/resolve"
-printf "%s\n" "nameserver 1.1.1.1" > "$TARGET/run/systemd/resolve/stub-resolv.conf"
+mountpoint "$1"
+test -d "$1"
+mount -t proc proc "$1/proc"
+mount -t sysfs sysfs "$1/sys"
+mount -t tmpfs run "$1/run"
+mkdir -p "$1/run/systemd/resolve"
+printf "%s\n" "nameserver 1.1.1.1" > "$1/run/systemd/resolve/stub-resolv.conf"
