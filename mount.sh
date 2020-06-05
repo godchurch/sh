a800477832ad3442148d8b815cb22a7261e085f2
godchurch <37278203+godchurch@users.noreply.github.com>
2020-06-04 23:46:21 -1000

    reconfigure

--- mount.sh
+++ mount.sh
@@ -1,26 +1,23 @@
 #!/bin/sh
 
-set -e
+set -e -x
 TARGET="${1%/}"
-test -z "$TARGET" && { printf "Usage: %s [chroot directory]" "$0"; exit 1; }
-set -x
-mountpoint -q "$TARGET"; test -d "$TARGET"
+test -n "$TARGET"
+mountpoint -q "$TARGET";
 mount -t proc proc "$TARGET/proc"
 mount -t sysfs sysfs "$TARGET/sys"
-mount --bind /dev "$TARGET/dev"
-
-if test -e "$TARGET/etc/resolv.conf"; then
-  mount -t tmpfs run "$TARGET/run"
-  RESOLV_CONF="$TARGET/etc/resolv.conf"
-  printf "%s\n" "nameserver 1.1.1.1" > "$TARGET/run/default-resolv.conf"
-  if test -L "$RESOLV_CONF"; then
-    RESOLV_CONF="$(readlink "$RESOLV_CONF")"
-    case "$RESOLV_CONF" in
-      /*) RESOLV_CONF="${TARGET}${RESOLV_CONF}" ;;
-      *) RESOLV_CONF="$TARGET/etc/$RESOLV_CONF" ;;
-    esac
-    RESOLV_CONF="${RESOLV_CONF%/}"
-    test -f "$RESOLV_CONF" || install -Dm644 /dev/null "$RESOLV_CONF"
-  fi
-  mount --bind "$TARGET/run/default-resolv.conf" "$RESOLV_CONF"
+mount -t tmpfs run "$TARGET/run"
+printf "%s\n" "nameserver 1.1.1.1" > "$TARGET/run/default-resolv.conf"
+RESOLV_CONF="$TARGET/etc/resolv.conf"
+if test -L "$RESOLV_CONF"; then
+  RESOLV_CONF="$(readlink "$RESOLV_CONF")"
+  case "$RESOLV_CONF" in
+    /*) RESOLV_CONF="${TARGET}${RESOLV_CONF}" ;;
+    *) RESOLV_CONF="$TARGET/etc/$RESOLV_CONF" ;;
+  esac
+  RESOLV_CONF="${RESOLV_CONF%/}"
+  test -f "$RESOLV_CONF" || install -Dm644 /dev/null "$RESOLV_CONF"
 fi
+test -e "$TARGET/etc/resolv.conf"
+mount --bind "$TARGET/run/default-resolv.conf" "$RESOLV_CONF"
+mount --bind /dev "$TARGET/dev"
