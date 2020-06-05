ef422e46984ce7b5839cc37903a352ae48c402cf
godchurch <37278203+godchurch@users.noreply.github.com>
2020-06-04 22:08:00 -1000

    handle resolv better

--- mount.sh
+++ mount.sh
@@ -1,13 +1,27 @@
 #!/bin/sh
 
 set -e
-test -z "$1" && { printf "Usage: %s [chroot directory]" "$0"; exit 1; }
+TARGET="${1%/}"
+test -z "$TARGET" && { printf "Usage: %s [chroot directory]" "$0"; exit 1; }
 set -x
-mountpoint "$1"
-test -d "$1"
-mount -t proc proc "$1/proc"
-mount -t sysfs sysfs "$1/sys"
-mount -t tmpfs run "$1/run"
-mount --bind /dev "$1/dev"
-mkdir -p "$1/run/systemd/resolve"
-printf "%s\n" "nameserver 1.1.1.1" > "$1/run/systemd/resolve/stub-resolv.conf"
+mountpoint "$TARGET"; test -d "$TARGET"
+mount -t proc proc "$TARGET/proc"
+mount -t sysfs sysfs "$TARGET/sys"
+mount -t tmpfs tmpfs "$TARGET/tmp"
+mount -t tmpfs run "$TARGET/run"
+mount --bind /dev "$TARGET/dev"
+
+if test -e "$TARGET/etc/resolv.conf"; then
+  RESOLV_CONF="$TARGET/etc/resolv.conf"
+  printf "%s\n" "nameserver 1.1.1.1" > "$TARGET/run/default-resolv.conf"
+  if test -L "$RESOLV_CONF"; then
+    RESOLV_CONF="$(readlink "$RESOLV_CONF")"
+    case "$RESOLV_CONF" in
+      /*) RESOLV_CONF="${TARGET}${RESOLV_CONF}" ;;
+      *) RESOLV_CONF="$TARGET/etc/$RESOLV_CONF" ;;
+    esac
+    RESOLV_CONF="${RESOLV_CONF%/}"
+    test -f "$RESOLV_CONF" || install -Dm644 /dev/null "$RESOLV_CONF"
+  fi
+  mount --bind "$TARGET/run/default-resolv.conf" "$RESOLV_CONF"
+fi
