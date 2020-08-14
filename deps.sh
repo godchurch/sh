65a4e7a7ea052e446698ff0b76b08007ea771567
godchurch <37278203+godchurch@users.noreply.github.com>
2020-08-13 16:52:59 -1000

    add packages

--- deps.sh
+++ deps.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
-set -e
-
 apt-get install \
   vim \
+  curl \
   git \
   make \
   tmux \
   qemu-kvm \
   libvirt-bin \
   bridge-utils \
+  virtualbox \
+  virtualbox-qt \
+  virtualbox-guest-additions-iso \
   keepassxc \
   bleachbit
