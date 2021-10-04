f6977df5adf029422815f03cc0e8885eac550abd
godchurch <37278203+godchurch@users.noreply.github.com>
2021-10-04 08:18:35 -1000

    cleaned up

--- setup_encrypted.sh
+++ setup_encrypted.sh
@@ -2,55 +2,66 @@
 # https://help.ubuntu.com/community/Full_Disk_Encryption_Howto_2019
 
 set -e
+set -x
+
+test "$#" -eq 1
+export DEVICE="$1"
+printf '%s' "$DEVICE_NAME" | grep -q 'nvme' && \
+  export DEVICE_NAME="${DEVICE##*/}p" || \
+  export DEVICE_NAME="${DEVICE##*/}"
 
 effectiveUserIdentity=`id -u`
 test "$effectiveUserIdentity" -eq 0
+
 mount | grep -q 'efivars'
 
-export DEV="/dev/nvme0n1"
-export DM="${DEV##*/}"
-export DM="${DM}$(if printf '%s' "$DM" | grep -q 'nvme'; then printf 'p'; fi)"
-
-sgdisk --zap-all "$DEV"
+sgdisk --zap-all "$DEVICE"
 sgdisk --new=1:0:+768M --typecode=1:8301 --change-name=1:/boot \
        --new=2:0:+2M --typecode=2:ef02 --change-name=2:GRUB \
        --new=3:0:+128M --typecode=3:ef00 --change-name=3:EFI-SP \
        --new=5:0:0 --typecode=5:8301 --change-name=5:rootfs \
-       "$DEV"
-sgdisk --hybrid 1:2:3 "$DEV"
+       "$DEVICE"
+sgdisk --hybrid 1:2:3 "$DEVICE"
 
-cryptsetup luksFormat --type=luks1 "${DEV}p1"
-cryptsetup luksFormat "${DEV}p5"
-cryptsetup open "${DEV}p1" "LUKS_BOOT"
-cryptsetup open "${DEV}p5" "${DM}5_crypt"
+cryptsetup luksFormat --type=luks1 "${DEVICE}p1"
+cryptsetup luksFormat "${DEVICE}p5"
+cryptsetup open "${DEVICE}p1" "LUKS_BOOT"
+cryptsetup open "${DEVICE}p5" "${DEVICE_NAME}5_crypt"
 
 mkfs.ext4 -L boot "/dev/mapper/LUKS_BOOT"
-mkfs.vfat -F 16 -n EFI-SP "${DEV}p3"
-mkfs.ext4 "/dev/mapper/${DM}5_crypt"
+mkfs.vfat -F 16 -n EFI-SP "${DEVICE}p3"
+mkfs.ext4 "/dev/mapper/${DEVICE}5_crypt"
 
 # run installer, manually configure partitions, setup user account...
-echo "################################################"
-echo "####            RUN INSTALLER               ####"
-echo "################################################"
+set +x; printf '%s\n' \
+  '################################################' \
+  '####            RUN INSTALLER               ####' \
+  '################################################'
 while [ ! -d /target/etc/default/grub.d ]; do sleep 1; done
-echo "GRUB_ENABLE_CRYPTODISK=y" >> /target/etc/default/grub
+printf 'GRUB_ENABLE_CRYPTODISK=y\n' >> /target/etc/default/grub
 
-exit 0
+printf '\n:: After the installer is done press Enter: '; read -r NULL
 
-# post installation:
+# Post Installation:
+set -x
 mount /dev/mapper/root /target
 for n in proc sys dev etc/resolv.conf; do mount --rbind /$n /target/$n; done
 chroot /target
 mount -a
-echo "KEYFILE_PATTERN=/etc/luks/*.keyfile" >> /etc/cryptsetup-initramfs/conf-hook
-echo "UMASK=0077" >> /etc/initramfs-tools/initramfs.conf
+printf 'KEYFILE_PATTERN=/etc/luks/*.keyfile\n' >> /etc/cryptsetup-initramfs/conf-hook
+printf 'UMASK=0077\n' >> /etc/initramfs-tools/initramfs.conf
 mkdir /etc/luks
 dd if=/dev/urandom of=/etc/luks/boot_os.keyfile bs=4096 count=1
 chmod u=rx,go-rwx /etc/luks
 chmod u=r,go-rwx /etc/luks/boot_os.keyfile
-cryptsetup luksAddKey ${DEV}p1 /etc/luks/boot_os.keyfile
-cryptsetup luksAddKey ${DEV}p5 /etc/luks/boot_os.keyfile
-echo "LUKS_BOOT UUID=$(blkid -s UUID -o value ${DEV}p1) /etc/luks/boot_os.keyfile luks,discard" >> /etc/crypttab
-echo "${DM}5_crypt UUID=$(blkid -s UUID -o value ${DEV}p5) /etc/luks/boot_os.keyfile luks,discard" >> /etc/crypttab
-# cat /etc/crypttab
+cryptsetup luksAddKey "${DEVICE}p1" /etc/luks/boot_os.keyfile
+cryptsetup luksAddKey "${DEVICE}p5" /etc/luks/boot_os.keyfile
+DEVICE_UUID_BOOT="`blkid -s UUID -o value "${DEVICE}p1"`"
+DEVICE_UUID_CRYPT="`blkid -s UUID -o value "${DEVICE}p5"`"
+printf 'LUKS_BOOT UUID=%s /etc/luks/boot_os.keyfile luks,discard\n' "$DEVICE_UUID_BOOT" >> /etc/crypttab
+printf '%s UUID=%s /etc/luks/boot_os.keyfile luks,discard\n' "${DEVICE_NAME}5_crypt" "$DEVICE_UUID_CRYPT" >> /etc/crypttab
 update-initramfs -u -k all
+set +x; printf '%s\n' \
+  '################################################' \
+  '####          DONE: YOU CAN REBOOT          ####' \
+  '################################################'
