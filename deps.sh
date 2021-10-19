d1c42195ada6abb906bc28ac0196bab59ce1fa5c
godchurch <37278203+godchurch@users.noreply.github.com>
2021-10-18 22:19:51 -1000

    update deps

--- deps.sh
+++ deps.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
 
-apt-get install \
-  vim \
-  curl \
-  git \
-  make \
-  tmux \
-  qemu-kvm \
-  libvirt-daemon \
-  libvirt-clients \
-  bridge-utils \
-  virtualbox \
-  virtualbox-qt \
-  virtualbox-guest-additions-iso \
-  keepassxc \
-  bleachbit
+if test `id -u` -ne 0; then
+	command -v sudo > /dev/null && exec sudo "$0" "$@"
+	printf 'E: Permission denied\n' >&2; exit 1
+fi
+
+exec apt-get -y install \
+	vim \
+	curl \
+	git \
+	make \
+	tmux \
+	qemu-system-x86 \
+	libvirt-daemon \
+	libvirt-clients \
+	bridge-utils \
+	virtualbox \
+	virtualbox-qt \
+	virtualbox-guest-additions-iso \
+	keepassxc \
+	bleachbit
