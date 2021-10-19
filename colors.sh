cc7a38d9a205d53ee249151cfcc74f1145d6533e
godchurch <37278203+godchurch@users.noreply.github.com>
2021-10-18 19:20:18 -1000

    use less commands, use more variables

--- colors.sh
+++ colors.sh
@@ -8,29 +8,27 @@
 # 40=black 41=red 42=green 43=yellow 44=blue 45=magenta 46=cyan 47=white
 
 for attribute in 00 01 04 05 07 08; do
-
-    case "$attribute" in
-        00) printf '> none       | ' ;;
-        01) printf '> bold       | ' ;;
-        04) printf '> underscore | ' ;;
-        05) printf '> blink      | ' ;;
-        07) printf '> reverse    | ' ;;
-        08) printf '> concealed  | ' ;;
-    esac
-
-    for color in 30 31 32 33 34 35 36 37; do
-        case "$color" in
-            30) printf "\033[${attribute};${color}m%s\033[0m " 'BLACK' ;;
-            31) printf "\033[${attribute};${color}m%s\033[0m " 'RED' ;;
-            32) printf "\033[${attribute};${color}m%s\033[0m " 'GREEN' ;;
-            33) printf "\033[${attribute};${color}m%s\033[0m " 'YELLOW' ;;
-            34) printf "\033[${attribute};${color}m%s\033[0m " 'BLUE' ;;
-            35) printf "\033[${attribute};${color}m%s\033[0m " 'MAGENTA' ;;
-            36) printf "\033[${attribute};${color}m%s\033[0m " 'CYAN' ;;
-            37) printf "\033[${attribute};${color}m%s\033[0m " 'WHITE' ;;
-        esac
-    done
-
-    printf '\n'
-
+	unset major minor
+	case "$attribute" in
+		00) major='NONE      ' ;;
+		01) major='BOLD      ' ;;
+		04) major='UNDERSCORE' ;;
+		05) major='BLINK     ' ;;
+		07) major='REVERSE   ' ;;
+		08) major='CONCEALED ' ;;
+	esac
+	for color in 30 31 32 33 34 35 36 37; do
+		case "$color" in
+			30) colorString="BLACK" ;;
+			31) colorString="RED" ;;
+			32) colorString="GREEN" ;;
+			33) colorString="YELLOW" ;;
+			34) colorString="BLUE" ;;
+			35) colorString="MAGENTA" ;;
+			36) colorString="CYAN" ;;
+			37) colorString="WHITE" ;;
+		esac
+		minor="${minor}\033[${attribute};${color}m${colorString} "
+	done
+	printf "    ${major} | ${minor}\033[0m\n"
 done
